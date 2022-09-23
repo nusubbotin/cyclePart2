6d1dc87f74e3e9afccb58dcda61749a548d14cfc
nusubbotin <nu.subbotin@ya.ru>
2022-09-23 17:16:26 +0300

    Циклы. Часть 2. Реализация требований по: Домашнее задание -1. Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        //Домашнее задание -3. Задача 2
-        int addSummMoney = 29000;
+        //Домашнее задание -1. Задача 1
+        int month = 0;
+        int addSummMoney = 15000;
         float accumMoney = 0;
-        System.out.println("Задание 9: ");
-        for (int i = 1; i <= 12 ; i++){
-
+        System.out.println("Задание 1: ");
+        while (accumMoney < 2_459_000) {
+            month++;
             accumMoney = accumMoney * 1.12f + addSummMoney;
-            System.out.println("Месяц "+ i +", сумма накоплений равна "+ accumMoney +" рублей");
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + accumMoney + " рублей");
         }
-
     }
 }
